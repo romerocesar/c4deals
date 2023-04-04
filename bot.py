d36a57edf958a1c7d3f96f1dd63d8602dcbc4d8a
cesar <cesar.romero@gmail.com>
2023-04-04 00:09:06 -0700

    initial notebook with bs4 code and bot draft

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+
+import discord
+
+
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+TARGET_USER = os.getenv('DISCORD_USER')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    user = await client.fetch_user(TARGET_USER)
+    deal = 'fake test deal! it sucks... '
+    await user.send(f'do you approve this {deal=}')
+
+client.run(BOT_TOKEN)
