37ae073e2fa20317edb6a98e6380525505be942f
cesar <cesar.romero@gmail.com>
2023-04-11 00:16:59 -0700

    updated bot to use a class and a yaml config file; fixed requirements.txt. implemented deals fetcher in slickdeals.py

--- bot.py
+++ bot.py
@@ -1,25 +1,33 @@
-import os
 import logging
 
 import discord
+import yaml
+
+from slickdeals import SlickDealsFetcher
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-TARGET_USER = os.getenv('DISCORD_USER')
+class C4Deals(discord.Client):
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.messages = True
+    def __init__(self, *args, config=None, **kwargs):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.messages = True
+        super().__init__(*args, intents=intents, **kwargs)
+        self.approvers = config.get('approvers')
+        self.token = config.get('token')
+        self.deals_fetcher = SlickDealsFetcher()
 
-client = discord.Client(intents=intents)
+    async def on_ready(self):
+        deals = self.deals_fetcher.fetch_deals()
+        for userid in self.approvers:
+            user = await client.fetch_user(userid)
+            deal = deals.iloc[0]
+            await user.send(f'do you approve this {deal=}')
 
 
-@client.event
-async def on_ready():
-    user = await client.fetch_user(TARGET_USER)
-    deal = 'fake test deal! it sucks... '
-    await user.send(f'do you approve this {deal=}')
+config = yaml.safe_load(open('config.yaml'))
+client = C4Deals(config=config)
 
-client.run(BOT_TOKEN)
+client.run(config.get('token'))
---
+++ slickdeals.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import logging
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+logger = logging.getLogger('c4deals')
+logging.basicConfig(level=logging.DEBUG)
+
+
+class SlickDealsFetcher:
+
+    def __init__(self, html=None):
+        self.url = "https://slickdeals.net/video-game-deals/"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+        self.html = html
+
+    def fetch_deals(self):
+        if self.html:
+            decoded = self.html
+        else:
+            response = requests.get(self.url, headers=self.headers)
+            decoded = response.content.decode("utf-8")
+            now = datetime.now().strftime("%Y-%m-%d-%H-%M-%s")
+            fname = f'{now}.html'
+            with open(fname, 'w') as fp:
+                fp.write(decoded)
+                logger.debug(f'wrote deals html into {fname=}')
+        soup = BeautifulSoup(decoded, "html.parser")
+        cards = soup.find_all("li", attrs={'data-catalogitem': "DealCard"})
+        deals = []
+        for card in cards:
+            try:
+                deals.append(SlickDealsFetcher._parse_card(card))
+            except Exception as e:
+                logger.warning(f'failed to parse {card=}. {e}')
+        df = pd.DataFrame(deals)
+        return df
+
+    @staticmethod
+    def _parse_card(card):
+        deal = dict()
+        deal['id'] = card.get('id')
+        deal['title'] = card.find('a', class_="bp-c-card_title").text
+        deal['price'] = card.find('span', class_="bp-p-dealCard_price").text
+        if original := card.find('span', class_='bp-p-dealCard_originalPrice'):
+            deal['original-price'] = original.text
+        deal['fire'] = bool(card.find('span', class_='bp-i-bbFire'))
+        deal['store'] = card.find('span', class_='bp-c-card_subtitle').text
+        deal['votes'] = card.find('span', class_='bp-p-votingThumbsPopup_voteCount').text
+        deal['frontpage'] = bool(card.find('span', class_='bp-c-label--frontpage'))
+        deal['popular'] = bool(card.find('span', class_='bp-c-label--popular'))
+        return deal
